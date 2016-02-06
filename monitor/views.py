a6160d82efd73b90a84add85a5fcba14907b17db
larsclaussen <lars.claussen@nelen-schuurmans.nl>
2016-02-06 16:57:21 +0100

    added js code and css

--- monitor/views.py
+++ monitor/views.py
@@ -32,3 +32,71 @@ def init_work(request):
     print 'add.state ', add_result.state
 
     return HttpResponseRedirect(reverse('poll_state') + '?job=' + job.id)
+
+
+<script type="text/javascript">
+  jQuery(document).ready(function() {
+
+   // pole state of the current task
+   var PollState = function(task_id) {
+    jQuery.ajax({
+     url: "poll_state",
+     type: "GET",
+     data: "task_id=" + task_id,
+    }).done(function(task){
+     console.log(task);
+     if (task.process_percent) {
+      jQuery('.bar').css({'width': task.process_percent + '%'});
+      jQuery('.bar').html(task.process_percent + '%')
+     } else {
+      jQuery('.status').html(task);
+     };
+
+     // create the infinite loop of Ajax calls to check the state
+     // of the current task
+     PollState(task_id);
+    });
+   }
+
+   PollState('{{ task_id }}');
+  });
+ </script>
+
+
+(function worker() {
+  $.ajax({
+    url: 'poll_state',
+    type: 'get'
+    success: function(data) {
+      $('.result').html(data);
+    },
+    complete: function() {
+      // Schedule the next request when the current one's complete
+      setTimeout(worker, 5000);
+    }
+  });
+})();
+
+
+#progressBar {
+    width: 400px;
+    height: 22px;
+    border: 1px solid #111;
+    background-color: #292929;
+}
+
+#progressBar div {
+    height: 100%;
+    color: #fff;
+    text-align: right;
+    line-height: 22px; /* same as #progressBar height if we want text middle aligned */
+    width: 0;
+    background-color: #0099ff;
+}
+
+function progress(percent, $element) {
+    var progressBarWidth = percent * $element.width() / 100;
+    $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "% ");
+}
+
+progress(80, $('#progressBar'));
